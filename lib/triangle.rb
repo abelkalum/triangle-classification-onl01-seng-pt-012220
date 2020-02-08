23d77f1ca723b7f99ef288bed672dc5bdf7fc2c2
Abel Kalum <chumbabel@gmail.com>
2020-02-08 02:58:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,41 +1,23 @@
 # lib/triangle.rb
  
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-  end
-
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
+  def initialize(*args)
+    @sides = *args.sort
   end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
+    raise TriangleError if @sides.any? {|s| s <= 0} or
+      @sides[0]+ @sides[1] <= @sides[2]
+    case @sides.uniq.size
+    when 1
+      :equilateral
+    when 2
+      :isosceles
     else
-      raise TriangleError
+      :scalene
     end
   end
 end
 
 class TriangleError < StandardError
-
 end
